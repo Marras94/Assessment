46fe27bbd3de1aa9df03923cdc82091d032bf4d1
Marthe M. Rasmussen <martherasmussen@hotmail.com>
2021-11-14 20:22:57 +0100

    Added more prints

--- try2.py
+++ try2.py
@@ -1,17 +1,21 @@
 import random
+
 class randomnumbers:
     def __init__(self):
-        print("Welcome to the random printer" )
+        print("Welcome!")
+        print("Printing out random numbers for you: ")
+
         try:
-            self.limit = int(input("How many numbers should be returned?: "))
+            self.limit = int(input("How many numbers do you wan't? "))
             self.min = int(input("Enter the lowest number: "))
             self.max = int(input("Enter the highest number: "))
-            self.retrive(self.limit, self.min, self.max)
-    
-        except:
-            print("Error, please try again!")
 
-    def retrive(self, limit, min, max):
+            self.retrive(self.limit, self.min, self.max)
+
+        except:
+            print("Error, please try again")
+
+    def retrive(self, limit,min,max):
         for i in range(limit):
             print(random.randint(min, max))
 
