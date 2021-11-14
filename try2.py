ceb62e63397f7a4729d1c9446723488e31684034
Marthe M. Rasmussen <martherasmussen@hotmail.com>
2021-11-14 20:19:50 +0100

    Error handling fixed

--- try2.py
+++ try2.py
@@ -1,12 +1,14 @@
 import random
 class randomnumbers:
     def __init__(self):
+        try:
+            self.limit = int(input("How many numbers should be returned?: "))
+            self.min = int(input("Enter the lowest number: "))
+            self.max = int(input("Enter the highest number: "))
+            self.retrive(self.limit, self.min, self.max)
 
-        self.limit = int(input("How many numbers should be returned?: "))
-        self.min = int(input("Enter the lowest number: "))
-        self.max = int(input("Enter the highest number: "))
-        self.retrive(self.limit, self.min, self.max)
-
+        except:
+            print("Error, please try again!")
     def retrive(self, limit, min, max):
         for i in range(limit):
             print(random.randint(min, max))
